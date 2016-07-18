ff9b27ddd4de45baf854ad95429ce7d63f326394
Juho Friman <juho.t.friman@gmail.com>
2016-07-18 20:11:01 +0300

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+'use strict'
+
+var React = require('react');
+var ReactDom = require('react-dom');
+var AddresBook = require('./AddressBook.jsx');
+
+ReactDom.render(<AddresBook />, document.getElementById('app-container'))
