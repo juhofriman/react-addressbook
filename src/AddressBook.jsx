e345097a8740535069da7a26e67dee191a5d7aad
Juho Friman <juho.friman@solita.fi>
2016-07-19 09:19:11 +0300

    Moved all the state to where it belongs

--- src/AddressBook.jsx
+++ src/AddressBook.jsx
@@ -13,15 +13,42 @@ module.exports  = React.createClass({
         {firstname: "John", lastname: "Keller", phone: "050-233456"},
         {firstname: "Mark", lastname: "Miller", phone: "050-3435353"},
         {firstname: "Ada", lastname: "Malkoller", phone: "050-262662"}],
-      hotsearch: '' };
+      hotsearch: '',
+      forms: {
+        addNewContact: {
+          firstname: '',
+          lastname: '',
+          phone: ''
+        }
+      }};
   },
   componentDidMount: function() {
+
     eb.on(this, 'HOTSEARCH_CHANGE', function(currentSearch) {
+      console.log('Dude! Hotsearch form change was received. Setting state.');
       this.setState({hotsearch: currentSearch});
     });
-    eb.on(this, 'CREATE_NEW_CONTACT', function(newContact) {
-      var c = this.state.contacts;
-      this.setState({contacts: c.concat([newContact])});
+
+    eb.on(this, 'CREATE_NEW_CONTACT', function() {
+      console.log("Create new contant event received. Great! Adding new contact from state <3 <3");
+      var newState = this.state;
+      // Add new contact to contacts
+      newState.contacts = newState.contacts.concat([this.state.forms.addNewContact]);
+      // Reset form!
+      newState.forms.addNewContact = {
+        firstname: '',
+        lastname: '',
+        phone: ''
+      };
+      // Set the state
+      this.setState(newState);
+    });
+
+    eb.on(this, 'ADD_NEW_CONTACT_FIELD_CHANGE', function(fieldKey, fieldValue) {
+      console.log("Awyea! State change in add new contact form! " + fieldKey + "=" + fieldValue);
+      var newState = this.state;
+      newState.forms.addNewContact[fieldKey] = fieldValue;
+      this.setState(newState);
     });
   },
   filteredContacts: function() {
@@ -50,7 +77,7 @@ module.exports  = React.createClass({
               <div className="all-50">
                 <div className="ink-form">
                   <h4>Add new contact</h4>
-                  <NewContactForm/>
+                  <NewContactForm data={this.state.forms.addNewContact}/>
                 </div>
               </div>
             </div>
--- src/NewContactForm.jsx
+++ src/NewContactForm.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
 var eb = require('./eventbus.js');
 
+var FormInput = React.createClass({
+  triggerChangeEvent: function() {
+    // Publish event signaling that field has changed so the state should be updated
+    eb.publish('ADD_NEW_CONTACT_FIELD_CHANGE', this.props.id, this.refs.fieldValue.value);
+  },
+  render: function() {
+    return (
+      <input ref="fieldValue" value={this.props.value} type={this.props.type} name={this.props.id} id={this.props.id} onChange={this.triggerChangeEvent}/>
+    );
+  }
+});
+
 module.exports  = React.createClass({
   addNewContact: function() {
-    var contact = { firstname: this.refs.firstname.value, lastname: this.refs.lastname.value, phone: this.refs.phone.value};
-    eb.publish('CREATE_NEW_CONTACT', contact);
+    // Notice how we don't need to read the data anymore.
+    // Trust the state!
+    eb.publish('CREATE_NEW_CONTACT');
   },
   render: function() {
     return (
@@ -12,19 +25,19 @@ module.exports  = React.createClass({
         <div className="control-group column-group">
             <label htmlFor="first-name" className="all-20">First name</label>
             <div className="control all-80">
-                <input type="text" ref="firstname" name="first-name" id="first-name"/>
+                <FormInput type="text" id="firstname" value={this.props.data.firstname}/>
             </div>
         </div>
         <div className="control-group column-group">
             <label htmlFor="first-name" className="all-20">Last name</label>
             <div className="control all-80">
-                <input type="text" ref="lastname" name="last-name" id="last-name"/>
+                <FormInput type="text" id="lastname" value={this.props.data.lastname}/>
             </div>
         </div>
         <div className="control-group column-group">
             <label htmlFor="phone" className="all-20">Phone</label>
             <div className="control all-80">
-                <input type="text" ref="phone" name="phone" id="phone"/>
+                <FormInput type="text" id="phone" value={this.props.data.phone}/>
             </div>
         </div>
         <button className="ink-button red push-right" onClick={this.addNewContact}>Add</button>
